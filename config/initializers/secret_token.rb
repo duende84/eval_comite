cd2a265b7a30a0672886c0aac01e58d5a1ff82ea
duende84 <andreselduende@gmail.com>
2013-04-27 12:35:25 -0500

    Inicio

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EvalComite::Application.config.secret_token = '379eb59b414cac30fbb4cdd6a9eec1ba38b5966d19c04f4206ef32b3505187934dc8108eb1cafc27016556d7444a0975e5375209cc2719c43a99a86597702aaf'
